ddfeaa3b166a9f63b95ed55d0f98603fb6e8cc6b
Daniel <daniel@Daniels-MacBook-Pro.local>
2023-08-18 20:07:41 -0400

    Initial commit plus .gitignore file

---
+++ Bearsweeper/BearsweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearsweeperApp.swift
+//  Bearsweeper
+//
+//  Created by Daniel on 2023-08-18.
+//
+
+import SwiftUI
+
+@main
+struct BearsweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
