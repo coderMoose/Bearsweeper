2d7237544cf7ed688fdcc2d9f65520cf8126dde4
Daniel <daniel@Daniels-MacBook-Pro.local>
2023-09-04 14:50:35 -0400

    Add main view UI

---
+++ Bearsweeper/Views/MainView.swift
@@ -0,0 +1,57 @@
+//
+//  MainView.swift
+//  Bearsweeper
+//
+//  Created by Daniel on 2023-09-04.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            navBar
+            Spacer()
+            ZStack {
+                newGameButton
+            }
+            Spacer()
+        }
+        .background(Color.beige)
+    }
+  
+    private var newGameButton: some View {
+        Button {
+            withAnimation {
+                // switch screen to game view
+            }
+        } label: {
+            Text("New Game")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.brown)
+                .cornerRadius(10)
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+        }
+    }
+    
+    private var navBar: some View {
+        HStack {
+            Spacer()
+            Text("BEARSWEEPER")
+                .font(.title)
+                .fontWeight(.black)
+                .foregroundColor(.yellow)
+            Spacer()
+        }
+        .padding()
+        .background(Color.brown)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
